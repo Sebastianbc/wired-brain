dcf9f943324a91bcf8faf12996717e4221bb71ef
jcorrea16 <jcorrea16@slb.com>
2024-07-23 11:54:21 -0500

    Fix PUT and DELETE methos logic

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Default http method for Flask's routes is GET.
 An empty string is evaluated to false.
 
 request.json retrieves the request body data sent to the API endpoint.
+
+curl is a command client to make http requests.
+
+To pass an argument with a space in it to a route, you need to URL-encode the space as %20
+
+Flask automatically ports to 5000
+
+curl commands are not interpreted exactly in a Powershell environment. When working in PowerShell is better to use the Invoke-WebRequest cmdlet. I found it impossible to execute a POST request using curl client in a PowerShell env, it worked only when I used Invoke-WebRequest command. However, if you want to user curl as a client, you should use the Windows command prompt terminal.
+
+Every time the server is restarted due to a change and save in the source code, data is started, How should it behaves when working with a database?
+
--- product-service/src/app.py
+++ product-service/src/app.py
@@ -7,6 +7,7 @@ products = [
 
 app = Flask(__name__)
 
+# curl http://localhost:5000/products
 # Default http method for Flask's routes is GET
 @app.route('/products')
 def get_products():
@@ -15,6 +16,7 @@ def get_products():
         return jsonify(products), 200
     return f'There are no products available', 404
 
+# curl http://localhost:5000/products/Mug%20brain
 @app.route('/products/<name>')
 def get_product(name):
     product = [product for product in products if product['name'] == name]
@@ -24,8 +26,10 @@ def get_product(name):
     
     return f'No product available with that information.', 404
 
+# For PowerShell terminal use: Invoke-WebRequest -Uri http://localhost:5000/product -Method POST -Content-Type "application/json" -Body '{"name": "Gigantic nose mug"}' 
+# For CMD terminal use: curl -X POST -Uri http://localhost:5000/product --header "Content-Type: application/json" --data "{\"name\": \"Fuzzy key\"}"
 @app.route('/product', methods=['POST'])
-def post_product(name):
+def post_product():
     #Retrieve the request data (product)
     request_product = request.json
 
@@ -41,25 +45,28 @@ def post_product(name):
     #Return the new product back
     return jsonify(new_product), 201
 
+# curl -X PUT http://localhost:5000/product/3 --header "Content-Type: application/json" --data "{\"name\": \"Super Shaker\"}"
 @app.route('/product/<int:id>', methods=['PUT'])
 def put_product(id):
     #Retrieve request json body
     update_product = request.json
-
+    print(update_product)
     for product in products: 
         if product['id'] == id:
-            product['name'] == update_product['name']
+            product['name'] = update_product['name']
             return jsonify(product), 200
         
     return f'Product with {id} id not found', 404
 
+# curl -X DELETE http://localhost:5000/product/Gig%20nose%20mug
 @app.route('/product/<name>', methods=['DELETE'])
 def delete_product(name):
     product = [product for product in products if product['name'] == name]
-
+    
     # If product is not null
     if product:
-        products.remove(product)
+        #Remove product from products list
+        products.remove(product[0])
         return f'{name} product has been removed', 200
     
     return f'{name} product not found', 404
